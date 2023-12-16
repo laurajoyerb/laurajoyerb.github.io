fab32b7fe5373442266d2d09489cdc1266d23f1c
Laura Joy Erb <laurajoyerb@gmail.com>
2023-12-16 14:29:10 +0100

    add prettier

--- lekker/src/Header.js
+++ lekker/src/Header.js
@@ -2,8 +2,14 @@
 
 import './root.css';
 
-import React from "react";
+import React from 'react';
 
 export default function Header(): React$Node {
-    return <img className="header" src="./images/header.png" alt="lekker cabinet logo" />;
+  return (
+    <img
+      className="header"
+      src="./images/header.png"
+      alt="lekker cabinet logo"
+    />
+  );
 }
