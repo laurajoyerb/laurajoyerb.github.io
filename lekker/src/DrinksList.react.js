71dbcd2f5d73574b16377a1a9265ece95c15aec8
Laura Joy Erb <laurajoyerb@gmail.com>
2023-12-23 16:42:13 -0800

    stylise drinks list

--- lekker/src/DrinksList.react.js
+++ lekker/src/DrinksList.react.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import type { Cocktail } from './Types';
+import { css } from '@emotion/css';
 
 type Props = $ReadOnly<{
   drinks: $ReadOnlyArray<Cocktail>,
@@ -9,10 +10,40 @@ type Props = $ReadOnly<{
 
 export default function DrinksList({ drinks }: Props): React$Node {
   return (
-    <ul>
+    <div className={styles.root}>
       {drinks.map((drink) => (
-        <li key={drink.name}>{drink.name}</li>
+        <div className={styles.drink} key={drink.name}>
+          <div className={styles.title}>{drink.name.toUpperCase()}</div>
+          <div className={styles.ingredients}>
+            {drink.ingredients.join(', ')}
+          </div>
+        </div>
       ))}
-    </ul>
+    </div>
   );
 }
+
+// Styles
+
+const styles = {
+  root: css`
+    padding: 16px 10px;
+    gap: 14px;
+    display: flex;
+    flex-direction: column;
+  `,
+  drink: css`
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+  `,
+  title: css`
+    color: var(--dark-purple);
+    font-weight: 500;
+    font-size: 16px;
+    letter-spacing: 0.1em;
+  `,
+  ingredients: css`
+    font-size: 14px;
+  `,
+};
