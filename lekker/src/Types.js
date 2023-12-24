d33b69d4c07e9e78a7ce49f1e1c2c8718dffd69d
Laura Joy Erb <laurajoyerb@gmail.com>
2023-12-23 18:23:11 -0800

    working on adding cocktails and ingredients types

--- lekker/src/Types.js
+++ lekker/src/Types.js
@@ -1,13 +1,33 @@
 // @flow
 
 export type Ingredient =
+  | 'Gin'
+  | 'sweet vermouth'
+  | 'campari'
+  | 'Gold rum'
   | 'lime juice'
-  | 'simple syrup'
-  | 'whiskey'
-  | 'lemon juice'
+  | 'grenadine'
+  | 'tequila'
+  | 'vodka'
   | 'rum'
+  | 'cointreau'
+  | 'simple syrup'
+  | 'lemon juice'
   | 'cola'
-  | 'grenadine';
+  | 'whiskey'
+  | 'angostura bitters'
+  | 'champagne'
+  | 'elderflower liqueur'
+  | 'club soda'
+  | 'Cointreau'
+  | 'dry vermouth'
+  | 'White rum'
+  | 'Irish cream liqueur'
+  | 'coffee liqueur'
+  | 'more dry vermouth'
+  | 'lillet blanc'
+  | 'St. Germain'
+  | 'sugar';
 
 export type Cocktail = {
   ingredients: $ReadOnlyArray<Ingredient>,
