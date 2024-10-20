4cd718d57db42ef332b76eb50ec24265b3047a5d
Laura Joy Erb <laurajoyerb@gmail.com>
2024-10-20 21:51:32 +0100

    adding chips for search (wip)

--- lekker/src/Menu.react.js
+++ lekker/src/Menu.react.js
@@ -2,13 +2,16 @@
 
 import React from 'react';
 import { cocktails } from './Cocktails';
-import type { Cocktail } from './Types';
+import type { Cocktail, Ingredient } from './Types';
 import DrinksList from './mobile/DrinksList.react';
 import { default as DesktopDrinksList } from './desktop/DesktopDrinksList.react';
 import { Tab, tabClasses } from '@mui/base/Tab';
 import { Tabs } from '@mui/base/Tabs';
 import { TabsList } from '@mui/base/TabsList';
 import { css } from '@emotion/css';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import AddIcon from '@mui/icons-material/Add';
 
 type MenuProps = $ReadOnly<{
   isDesktop?: boolean,
@@ -16,6 +19,24 @@ type MenuProps = $ReadOnly<{
 
 export default function Menu({ isDesktop = false }: MenuProps): React$Node {
   const [tab, setTab] = React.useState('Signature');
+  const [selectedIngredients, setSelectedIngredients] = React.useState<
+    $ReadOnlyArray<Ingredient>,
+  >([]);
+  const [unselectedIngredients, setUnselectedIngredients] = React.useState<
+    $ReadOnlyArray<Ingredient>,
+  >(['whiskey', 'gin', 'rum', 'tequila', 'vodka']);
+
+  const handleAddIngredient = (ingredient: Ingredient) => {
+    setSelectedIngredients([...selectedIngredients, ingredient]);
+    setUnselectedIngredients(
+      unselectedIngredients.filter((i) => i !== ingredient),
+    );
+  };
+
+  const handleRemoveIngredient = (ingredient: Ingredient) => {
+    setSelectedIngredients(selectedIngredients.filter((i) => i !== ingredient));
+    setUnselectedIngredients([...unselectedIngredients, ingredient]);
+  };
 
   const filteredDrinks = useFilteredDrinks(tab);
 
@@ -37,6 +58,29 @@ export default function Menu({ isDesktop = false }: MenuProps): React$Node {
           },
         )}
       </TabsList>
+      {/* {tab === 'Others' && (
+        <Stack className={styles.ingredients} direction="row" spacing={1}>
+          {selectedIngredients.map((ingredient) => {
+            return (
+              <Chip
+                key={ingredient}
+                label={ingredient}
+                onDelete={() => handleRemoveIngredient(ingredient)}
+              />
+            );
+          })}
+          {unselectedIngredients.map((ingredient) => {
+            return (
+              <Chip
+                key={ingredient}
+                label={ingredient}
+                icon={<AddIcon />}
+                onClick={() => handleAddIngredient(ingredient)}
+              />
+            );
+          })}
+        </Stack>
+      )} */}
       {isDesktop ? (
         <DesktopDrinksList drinks={filteredDrinks} tab={tab} />
       ) : (
@@ -105,5 +149,10 @@ const styles = {
   `,
   tabOuter: css`
     display: flex;
+  `,
+  ingredients: css`
+    max-width: 85%;
+    margin: 0 auto;
+    overflow-x: scroll;
   `,
 };
